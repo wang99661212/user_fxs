2a24d2f1dcf1fcc94d3f926073f2db0d99d17a46
wangyongzheng <2684192572@qq.com>
2021-08-25 20:00:04 +0800

    8.25提交

--- src/views/xiaowang.vue
+++ src/views/xiaowang.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="list">
     <div class="list_top">
-      <!-- <img src="../../public/img/logo.png" class="logo_img"> -->
+      <img src="../assets/切图/logo.png" class="logo_img">
       <p style="font-size:28px; margin-top:20px; margin-left:45px;">效果图</p>
       <div class="list_top_right">
         <div class="right_one">
-          <!-- <img src="../../public/img/my.png" width="38px" height="38px"> -->
-          <p style="font-size:16px; margin-left:3px; color:#999999;">我的</p>
+          <i class="icon iconfont icon-user" style="font-size:25px;"></i>
+          <p style="font-size:16px;  color:#999999;width:40px;">我的</p>
         </div>
         <div class="right_two">
-          <!-- <img src="../../public/img/gang.png" width="38px" height="38px"> -->
-          <p style="font-size:16px; margin-left:3px; color:#999999;">导航</p>
+          <i class="icon iconfont icon-nav" style="font-size:25px;"></i>
+          <p style="font-size:16px;  color:#999999;width:40px;">导航</p>
         </div>
       </div>
     </div>
@@ -80,6 +80,178 @@
         <p style="font-size:15px;">欧美文化的抽象美感</p>
         <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
       </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+      <div class="box_every_two">
+        <img
+          src="http://jd.itying.com/upload/aZiSoT5NZ4n454ipYZqLgsr8.png"
+          width="100%"
+          height="150px"
+        >
+        <p style="font-size:15px;">欧美文化的抽象美感</p>
+        <p style="font-size:15px;">混搭&emsp;三室&emsp;1100元/m</p>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="page">
+        <div class="page_one">上一页</div>
+        <div class="page_two">2/30</div>
+        <div class="page_three">下一页</div>
+      </div>
+      <div
+        class="txt"
+      >房先森互联网家装有别于传统装修公司与其他装修中介平台，以自营模式整合设计师与项目经理，杜绝项目转包外包，利用互联网将工地进行在线全景直播，通过去中间环节，提供装修套餐+个性化定制服务。</div>
+      <div class="fuwu">
+        <div class="fuwu_top">
+          <i class="icon iconfont icon-hot-line" style="font-size:30px; color:#39c894;"></i>
+          <span style="font-size:20px;">服务热线:400-9977-658</span>
+        </div>
+        <div class="fuwu_bottom">
+          <p>触屏版</p>
+          <p>电脑版</p>
+          <p>关于我们</p>
+          <p>联系我们</p>
+        </div>
+      </div>
+      <div class="baojia">
+        <img src="../assets/切图/111.jpg" width="100%" height="100%">
+      </div>
+      <p class="p">
+        <i class="icon iconfont icon-top" style="height:60px;width:40px;color:#999999;"></i>
+      </p>
     </div>
   </div>
 </template>
@@ -99,12 +271,14 @@ export default {
 .list {
   width: 100%;
   height: 100%;
+  background: #eaeaea;
 }
 .list_top {
   width: 100%;
   height: 150px;
   display: flex;
   border-bottom: 1px solid #999999;
+  background: #ffffff;
 }
 .list_huxing {
   width: 100%;
@@ -113,6 +287,7 @@ export default {
   border-bottom: 1px solid #999999;
   padding-left: 40px;
   box-sizing: border-box;
+  background: #ffffff;
 }
 .logo_img {
   width: 200px;
@@ -127,7 +302,7 @@ export default {
   margin-top: 17px;
 }
 .right_two {
-  margin-left: 10px;
+  margin-left: 15px;
 }
 .huxing_one {
   width: 200px;
@@ -169,9 +344,9 @@ export default {
 }
 .list_box {
   width: 100%;
-  background: #eaeaea;
   display: flex;
   flex-wrap: wrap;
+  background: #eaeaea;
 }
 .box_every {
   width: 363px;
@@ -187,6 +362,92 @@ export default {
   text-align: center;
   margin-top: 5px;
   margin-left: 22px;
+}
+.footer {
+  width: 100%;
+  background: #eaeaea;
+  padding-top: 50px;
+  box-sizing: border-box;
+}
+.page {
+  width: 100%;
+  height: 82px;
+  background: #ffffff;
+  font-size: 35px;
+  display: flex;
+}
+.page_one {
+  width: 33%;
+  height: 100%;
+  background: #39c894;
+  text-align: center;
+  line-height: 82px;
+  color: #ffffff;
+}
+.page_two {
+  width: 33%;
+  height: 100%;
+  text-align: center;
+  line-height: 82px;
+}
+.page_three {
+  width: 33%;
+  height: 100%;
+  background: #39c894;
+  text-align: center;
+  line-height: 82px;
+  color: #ffffff;
+}
+.txt {
+  width: 100%;
+  height: 300px;
+  background: #ffffff;
+  margin-top: 50px;
+  text-indent: 2em;
+  font-size: 35px;
+  padding-top: 35px;
+  box-sizing: border-box;
+}
+.fuwu {
+  width: 100%;
+  height: 190px;
+  background: #ffffff;
+  margin-top: 50px;
+}
+.fuwu_top {
+  width: 100%;
+  height: 120px;
+  text-align: center;
+  padding-top: 20px;
+  box-sizing: border-box;
+}
+.fuwu_bottom {
+  width: 100%;
+  height: 70px;
+  display: flex;
+}
+.fuwu_bottom p {
+  width: 25%;
+  height: 100%;
+  text-align: center;
+  font-size: 30px;
+}
+.baojia {
+  width: 100%;
+  height: 185px;
+  background: #ffffff;
+  margin-top: 50px;
+}
+.p {
+  width: 90px;
+  height: 90px;
+  border: 1px solid #999999;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 90px;
+  position: absolute;
+  right: 3px;
+  top: 60.666667rem;
 }
 </style>
 //                       .::::.
