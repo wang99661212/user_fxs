8e0b4638e23a02b0636cefa78163115b08ee7339
renbaili <1317988372@qq.com>
2021-08-25 08:01:40 +0800

    完成了头部就提交了nb666

---
+++ src/views/baili.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <header>
+      <img src="@/assets/切图/logo.png" alt="" />
+      <p style="font-size:25px;">
+        装修流程
+      </p>
+      <div class="head_icon">
+        <div class="head_icon_right">
+          <p>
+            <i
+              class=" inco iconfont icon-nav"
+              style="font-size:20px;color:gray;"
+            ></i>
+          </p>
+          <span style="font-size:20px;display:block">我的</span>
+        </div>
+        <div class="head_icon_right">
+          <p>
+            <i
+              class="inco iconfont icon-user"
+              style="font-size:20px;color:gray;"
+            ></i>
+          </p>
+          <span style="font-size:20px;display:block">导航</span>
+        </div>
+      </div>
+    </header>
+    <div class="con"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+header {
+  img {
+    width: 250px;
+    height: 90px;
+  }
+  .head_icon {
+    display: flex;
+    justify-content: space-between;
+  }
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
